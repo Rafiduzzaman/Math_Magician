ae81e758492321ab5559d4f84f0dbf26bf457546
codeStackr <rdzaman20000@gmail.com>
2023-08-26 07:21:20 +0600

    Added API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Calculator from './Components/Calculator';
+import QuoteDisplay from './Components/QuoteDisplay';
 
 const App = () => (
   <div className="app">
     <Calculator />
+    <QuoteDisplay />
   </div>
 );
 
